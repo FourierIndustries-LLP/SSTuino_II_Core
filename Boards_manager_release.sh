f0be546e9b6a1ffdabc05973a0eaa05b40902e07
Pan Ziyue <random.rrr3@gmail.com>
2025-01-15 15:13:38 +0800

    hotfix patch to package name to avoid bug in Arduino IDE 2.3.3

--- Boards_manager_release.sh
+++ Boards_manager_release.sh
@@ -108,7 +108,7 @@ jq -r                                        \
       "version": "7.3.0-atmel3.6.1-arduino7"
     },
     {
-      "packager": "SSTuino II Series Boards",
+      "packager": "FourierIndustries",
       "name": "avrdude",
       "version": "7.1-arduino.1"
     },
